e210229592137373752570a5c25d80416e9e9c36
rasfan <r.sahafamin@gmail.com>
2019-01-07 22:24:53 +0330

    register and login controller with routes added

---
+++ app/http/controllers/auth/loginController.js
@@ -0,0 +1,9 @@
+const controller = require('app/http/controllers/controller');
+
+class loginController extends controller {
+    showLoginForm(req, res) {
+        res.render('auth/login');
+    }
+}
+
+module.exports = new loginController();
---
+++ app/http/controllers/auth/registerController.js
@@ -0,0 +1,9 @@
+const controller = require('app/http/controllers/controller');
+
+class registerController extends controller {
+    showRegsitrationForm(req, res) {
+        res.render('auth/register');
+    }
+}
+
+module.exports = new registerController();
