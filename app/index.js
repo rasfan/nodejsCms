f74999a9a318ccf5a6c0341b7ed53c4b51650454
rasfan <r.sahafamin@gmail.com>
2019-01-07 17:54:22 +0330

    configuration

---
+++ app/index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const validator = require('express-validator');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const passport = require('passport');
+
+module.exports = class Application {
+    constructor() {
+        this.setupExpress();
+        this.setMongoConnection();
+        this.setConfig();
+    }
+
+    setupExpress() {
+        const server = http.createServer(app);
+        server.listen(3000, () => console.log('Listening on port 3000'));
+    }
+
+    setMongoConnection() {
+        mongoose.Promise = global.Promise;
+        mongoose.connect(
+            'mongodb://localhost/nodejscms',
+            { useNewUrlParser: true }
+        );
+    }
+
+    setConfig() {
+        app.use(express.static('public'));
+        app.set('view engine', 'ejs');
+        app.set('views', path.resolve('./resource/views'));
+
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(validator());
+        app.use(
+            session({
+                secret: 'mysecretkey',
+                resave: true,
+                saveUninitialized: true,
+                store: new MongoStore({
+                    mongooseConnection: mongoose.connection
+                })
+            })
+        );
+        app.use(cookieParser('mysecretkey'));
+        app.use(flash());
+
+        app.get('/', (req, res) => {
+            res.json('Hell world');
+        });
+    }
+};
