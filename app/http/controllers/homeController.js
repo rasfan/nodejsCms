4830faa6c583dc1d03682ea91ff631e75323ef8e
rasfan <r.sahafamin@gmail.com>
2019-01-07 22:16:35 +0330

    header and footer added

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -2,11 +2,7 @@ const controller = require('app/http/controllers/controller');
 
 class homeController extends controller {
     index(req, res) {
-        res.json(this.message());
-    }
-
-    message() {
-        return 'Home Page';
+        res.render('home');
     }
 }
 
